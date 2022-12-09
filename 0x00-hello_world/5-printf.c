cb3188f86a73dbae72899e7cc19582dfdf342291
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-09 03:56:25 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,10 @@
-#!/bin/bash
-
-/#include <stdio.h>
-
-
-
+#nclude < stdio.h >
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - Entry
+ * Return: Always 0
+ */
 int main(void)
 {
-	puts("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
-}
+}	
