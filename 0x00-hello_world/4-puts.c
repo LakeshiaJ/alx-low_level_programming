1417874308343feeb52a5640fcaacb76eda910f2
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-08 21:37:00 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-/**
- * main-Entry point
+/*
+ * main - print a line of code using puts
  *
- * Return:Always 0 (success)
+ * Return: o
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
-        
-        }
+	return (o);
+}	
