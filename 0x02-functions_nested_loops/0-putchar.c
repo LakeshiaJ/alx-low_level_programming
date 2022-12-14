087ab3eb7596c2b8ef0531828bdfe8eb2aa22557
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-14 00:30:29 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints _putchar and a new line
+ * main - Entry point
  * Return: Always 0
  */
 int main(void)
@@ -15,6 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 
+}
