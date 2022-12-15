9bddd874072a937caa65cadb66035e5b6ce3cd95
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-15 02:25:24 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-int i;
+int i; 
 
 printf("infinite loop incoming :(\n");
 
@@ -15,7 +15,7 @@ i = 0;
 /*
  * while (i < 10)
  * {
- *
+ *	putchar(i);
  * }
  */
 printf("infinite loop avoided! \\o/\n");
