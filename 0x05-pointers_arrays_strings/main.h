90a311a249827855afa8dd12761022e0474705aa
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-19 11:02:20 -0800

    task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,7 +9,5 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+char *_strcpy(char *dest, char *src)
 
-#endif /* MAIN_H*/
