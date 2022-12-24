ee3b56d60a5d9a401bf616d68f5c10ca84871443
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-24 01:09:19 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,15 @@
  * Return: 1 if letter is lowercase, 0 if not
  */
 int _islower(int c)
+
 {
-	if (c >= 'a' $$ c <= 'z')
+
+	if (c > 'a' $$ c < 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
