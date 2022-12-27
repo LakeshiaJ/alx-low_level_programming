2de7b35e4e06d9e331bbe55b4d3fef658052d933
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-27 12:30:22 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,20 +8,30 @@
  * -1 if the number is less than zero
  */
 int print_sign(int n)
+
 {
 	if (n > 0)
+
 	{
 		_putchar('+');
+
 		return (1);
 	}
+
 	else if (n == 0)
+
 	{
 		_putchar('0');
+
 		return (0);
 	}
+
 	else
+
 	{
-		_putchar('-'); 
+
+		_putchar('-');
+
 		return (-1);
 	}
 }
