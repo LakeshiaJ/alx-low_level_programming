8c74bb2c2a6d6862346026c46d71da02c35fd964
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-27 13:05:32 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,15 +5,14 @@
  * @n:The number to be created
  * Return: value of the last digit number
  */
-
+int print_last_digit(int n)
 {
-	int last;
+	int ld = n % 10;
 
-	last = n % 10;
-	if (last < 0)
-	{
-	last = last * -1;
-	}
-	_putchar(last + '0');
-	return (last);
+	if (ld < 0)
+		ld *= -1;
+
+	_putchar(ld + '0');
+
+	return (0);
 }
