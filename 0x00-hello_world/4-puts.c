619625e52361e40300a42cac6a751cc2e6a033c7
LakeshiaJ <joseph.mayaka@yahoo.com>
2022-12-29 19:58:43 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,12 @@
 /**
  * main - print a line of code using puts
  *
- * Return: O 
+ * Return:o
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+
+	return(0);
+
+}
