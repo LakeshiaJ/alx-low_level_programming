5ec0f88ab4b355983a2b92bb1f1fc876203edbd2
LakeshiaJ <joseph.mayaka@yahoo.com>
2023-01-15 17:18:48 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
 
-	while (i < n)
+	int i = 0;
+
+	for (; n > 0; i++)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b
+		n--;
 	}
 	return (s);
- }	
+}
