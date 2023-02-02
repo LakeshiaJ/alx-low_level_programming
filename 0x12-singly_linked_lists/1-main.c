995679d1163b9cd8282f10829e731c6dc48cdd20
LakeshiaJ <joseph.mayaka@yahoo.com>
2023-02-01 22:01:02 -0800

    0-print_list.c

--- 0x12-singly_linked_lists/1-main.c
+++ 0x12-singly_linked_lists/1-main.c
@@ -1,47 +1,56 @@
 #include <stdlib.h>
-
 #include <string.h>
-
 #include <stdio.h>
-
 #include "lists.h"
-
-
-
 /**
- *
- *  * main - check the code
- *
- *   *
+ * main - check the code
  *
  *    * Return: Always 0.
  *
  *     */
-
 int main(void)
-
 {
 
-	    list_t *head;
+    list_t *head;
 
-	        list_t *new;
+    list_t *new;
 
-		    list_t hello = {"World", 5, NULL};
+    list_t hello = {"World", 5, NULL};
 
-		        size_t n;
+    size_t n;
 
 
 
-			    head = &hello;
+    head = &hello;
 
-			        new = malloc(sizeof(list_t));
+    new = malloc(sizeof(list_t));
 
-				    if (new == NULL)
+    if (new == NULL)
 
-					        {
+    {
+    
+            printf("Error\n");
+    
+            return (1);
+    
+        }
 
-							        printf("Error\n");
+    new->str = strdup("Hello");
 
-								        return (1);
+    new->len = 5;
 
-									    }
+    new->next = head;
+
+    head = new;
+
+    n = list_len(head);
+
+    printf("-> %lu elements\n", n);
+
+    free(new->str);
+
+    free(new);
+
+    return (0);
+
+}
