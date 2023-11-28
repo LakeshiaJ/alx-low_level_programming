5701574f3d271b404c2b9c085e0ca0327d98951c
joseph <joseph.mayaka@yahoo.com>
2023-11-27 23:34:02 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,37 +1,31 @@
 #include "main.h"
-
 /**
- *  create_file - creates a file
- *   @filename: filename.
- *   @text_content: content writed in the file.
- *
- *   Return: 1 if it success. -1 if it fails.
- */
+  * create_file - creates a file.
+  * @filename: pointer to filename.
+  * @text_content: string to be written to file.
+  *
+  * Return:  1 on success -1 on failure.
+  */
 int create_file(const char *filename, char *text_content)
 {
-	nt fd;
-	int nletters;
-	int rwr;
+	int file, i, wr;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
-
+	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	if (text_content)
+	{
+		i = 0;
+		while (text_content[i])
+			i++;
+		wr = write(file, text_content, i);
+		if (wr != i)
+			return (-1);
+	}
 
-	for (nletters = 0; text_content[nletters]; nletters++)
-
-	if (rwr == -1)
-		return (-1);
-
-	close(fd);
-
+	close(file);
 	return (1);
 }
-
-
